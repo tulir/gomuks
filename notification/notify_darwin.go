1e52d5708d5ff407b28539b346f1c9580dae45e8
Tulir Asokan <tulir@maunium.net>
2018-03-20 22:24:50 +0200

    Add very simple cross-platform notification package

---
+++ notification/notify_darwin.go
@@ -0,0 +1,46 @@
+// gomuks - A terminal Matrix client written in Go.
+// Copyright (C) 2018 Tulir Asokan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package notification
+
+import "os/exec"
+
+var TerminalNotifierAvailable = false
+
+func init() {
+	if err := exec.Command("which", "terminal-notifier").Run(); err != nil {
+		TerminalNotifierAvailable = false
+	}
+	TerminalNotifierAvailable = true
+}
+
+func Send(title, text string, critical bool) error {
+	if TerminalNotifierAvailable {
+		args := []string{"-title", "gomuks", "-subtitle", title, "-message", text}
+		if critical {
+			args = append(args, "-timeout", "30")
+		}
+//		if len(iconPath) > 0 {
+//			args = append(args, "-appIcon", iconPath)
+//		}
+		return exec.Command("terminal-notifier", args...).Run()
+	}
+	title = strings.Replace(title, `"`, `\"`, -1)
+	text = strings.Replace(text, `"`, `\"`, -1)
+	notification := fmt.Sprintf("display notification \"%s\" with title \"%s\" subtitle \"%s\"", text, o.AppName, title)
+	return exec.Command("osascript", "-e", notification).Run()
+}
+
---
+++ notification/notify_windows.go
@@ -0,0 +1,34 @@
+// gomuks - A terminal Matrix client written in Go.
+// Copyright (C) 2018 Tulir Asokan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package notification
+
+import "gopkg.in/toast.v1"
+
+func Send(title, text string, critical bool) error {
+	notification := toast.Notification{
+		AppID: "gomuks",
+		Title: title,
+		Message: message,
+		Audio: toast.IM,
+		Duration: toast.Short,
+//		Icon: ...,
+	}
+	if critical {
+		notification.Duration = toast.Long
+	}
+	return notification.Push()
+}
