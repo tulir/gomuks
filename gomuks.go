9ff328c080763ac2f5aa1464d7138e482fe268ff
Tulir Asokan <tulir@maunium.net>
2021-02-19 18:18:53 +0200

    Bump version to 0.2.3

--- gomuks.go
+++ gomuks.go
@@ -56,7 +56,7 @@ func NewGomuks(uiProvider ifc.UIProvider, configDir, dataDir, cacheDir, download
 }
 
 func (gmx *Gomuks) Version() string {
-	return "v0.2.2"
+	return "v0.2.3"
 }
 
 // Save saves the active session and message history.
