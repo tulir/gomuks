de318d138e9e594486d2322db283c1d0c0bb6a28
retrotails <10767978+retrotails@users.noreply.github.com>
2022-01-27 06:36:18 -0500

    set topic line to the room name, when topic is empty

--- matrix/rooms/room.go
+++ matrix/rooms/room.go
@@ -476,6 +476,9 @@ func (room *Room) GetTopic() string {
 		if topicEvt != nil {
 			room.topicCache = topicEvt.Content.AsTopic().Topic
 		}
+		if room.topicCache == "" {
+			room.topicCache = room.NameCache
+		}
 	}
 	return room.topicCache
 }
