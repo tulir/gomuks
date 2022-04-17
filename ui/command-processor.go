1e6174f828802e9b3e4b25d4cc0e8f6ee7f4e8e1
Tulir Asokan <tulir@maunium.net>
2022-04-17 23:54:52 +0300

    Add test panic command

--- ui/command-processor.go
+++ ui/command-processor.go
@@ -168,6 +168,9 @@ func NewCommandProcessor(parent *MainView) *CommandProcessor {
 			"hprof":      cmdHeapProfile,
 			"cprof":      cmdCPUProfile,
 			"trace":      cmdTrace,
+			"panic": func(cmd *Command) {
+				panic("hello world")
+			},
 
 			"rainbownotice": cmdRainbowNotice,
 
