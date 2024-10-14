a4d1a7feeb3a79ecd494c35870273228a8d8a07c
Tulir Asokan <tulir@maunium.net>
2024-10-15 02:31:22 +0300

    build.sh: add build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+go generate ./web
+export MAUTRIX_VERSION=$(cat go.mod | grep 'maunium.net/go/mautrix ' | head -n1 | awk '{ print $2 }')
+go build -ldflags "-X main.Tag=$(git describe --exact-match --tags 2>/dev/null) -X main.Commit=$(git rev-parse HEAD) -X 'main.BuildTime=`date -Iseconds`' -X 'maunium.net/go/mautrix.GoModVersion=$MAUTRIX_VERSION'" "$@" || exit 2
