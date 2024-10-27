4968dcc8d24e383c1f4d19b045f50845346a5dee
Tulir Asokan <tulir@maunium.net>
2024-10-27 12:49:37 +0200

    web/emoji: wrap reaction shortcode in ::

--- web/src/util/emoji/index.ts
+++ web/src/util/emoji/index.ts
@@ -118,7 +118,7 @@ export function emojiToReactionContent(emoji: PartialEmoji, evtID: EventID): Rea
 		},
 	}
 	if (emoji.u?.startsWith("mxc://") && emoji.n) {
-		content["com.beeper.reaction.shortcode"] = emoji.n
+		content["com.beeper.reaction.shortcode"] = `:${emoji.n}:`
 	}
 	return content
 }
