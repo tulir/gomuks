8ff5fff1de177ec526198988b77ca9f4a94e5da4
Tulir Asokan <tulir@maunium.net>
2024-11-17 16:58:52 +0200

    web/vite: split node modules and emojis into separate files

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,17 @@ export default defineConfig({
 	base: "./",
 	build: {
 		target: ["esnext", "firefox128"],
+		rollupOptions: {
+			output: {
+				manualChunks: id => {
+					if (id.includes("node_modules") && !id.includes("katex")) {
+						return "vendor"
+					} else if (id.endsWith("/emoji/data.json")) {
+						return "emoji"
+					}
+				},
+			},
+		},
 	},
 	plugins: [
 		react(),
