52dbff6a84f092910e1f0dd83089c11cd787d131
Sumner Evans <sumner.evans@automattic.com>
2024-11-19 11:20:28 -0700

    build: user /usr/bin/env
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 go generate ./web
 export MAUTRIX_VERSION=$(cat go.mod | grep 'maunium.net/go/mautrix ' | head -n1 | awk '{ print $2 }')
 go build -ldflags "-X main.Tag=$(git describe --exact-match --tags 2>/dev/null) -X main.Commit=$(git rev-parse HEAD) -X 'main.BuildTime=`date -Iseconds`' -X 'maunium.net/go/mautrix.GoModVersion=$MAUTRIX_VERSION'" ./cmd/gomuks "$@" || exit 2
