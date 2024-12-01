b4e7efa799cc765feb509d97405c59bf29c6dbb0
Sumner Evans <sumner.evans@automattic.com>
2024-12-01 12:49:32 -0700

    flake: add devenv for Nix (#529)
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Gomuks development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.permittedInsecurePackages = [ "olm-3.2.16" ];
+        };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              glib-networking
+              go-task
+              go-tools
+              gotools
+              gst_all_1.gstreamer
+              gst_all_1.gst-plugins-base
+              gst_all_1.gst-plugins-good
+              gst_all_1.gst-plugins-bad
+              gst_all_1.gst-plugins-ugly
+              gst_all_1.gst-libav
+              gst_all_1.gst-vaapi
+              libsoup
+              olm
+              pkg-config
+              pre-commit
+              webkitgtk_4_1
+            ];
+          };
+        };
+      }));
+}
